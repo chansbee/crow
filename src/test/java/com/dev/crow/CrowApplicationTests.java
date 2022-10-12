65a9bb40acf7250a07664eb7a5b2cbf0dcb14bae
lee-kangchan <lsc4237@gmail.com>
2022-10-12 10:17:27 +0900

    Initial commit

---
+++ src/test/java/com/dev/crow/CrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.crow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
